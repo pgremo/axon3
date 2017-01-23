dbf01b121f438bd87f987867adc9c3ae757a6e40
Patrick Gremo <patrick.gremo@gmail.com>
2017-01-23 10:10:56 -0500

    added integration bean names to channels.  organization

--- src/main/java/spike/Application.java
+++ src/main/java/spike/Application.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan
-@EnableJms
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
